9a3fe9cb4592eb8287b723727a7eaae30e3d43d8
Rafael Alves <rafael.alves.proff@gmail.com>
2018-08-17 12:57:06 +0100

    Programar uma textBox para aceitar apenas algarismos e uma outra que irá aceitar apenas letras e espaços.

---
+++ soAlgarismosVSsoLetrasV2/soAlgarismosVSsoLetrasV2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace soAlgarismosVSsoLetrasV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_algarismos = new System.Windows.Forms.TextBox();
+            this.txt_letras = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_algarismos
+            // 
+            this.txt_algarismos.Location = new System.Drawing.Point(15, 29);
+            this.txt_algarismos.Name = "txt_algarismos";
+            this.txt_algarismos.Size = new System.Drawing.Size(100, 22);
+            this.txt_algarismos.TabIndex = 0;
+            this.txt_algarismos.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txt_algarismos.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_algarismos_KeyPress);
+            // 
+            // txt_letras
+            // 
+            this.txt_letras.Location = new System.Drawing.Point(15, 84);
+            this.txt_letras.Name = "txt_letras";
+            this.txt_letras.Size = new System.Drawing.Size(100, 22);
+            this.txt_letras.TabIndex = 1;
+            this.txt_letras.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_letras_KeyPress_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Só algarismos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Só Letras";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(178, 153);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_letras);
+            this.Controls.Add(this.txt_algarismos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_algarismos;
+        private System.Windows.Forms.TextBox txt_letras;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ soAlgarismosVSsoLetrasV2/soAlgarismosVSsoLetrasV2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace soAlgarismosVSsoLetrasV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /*
+         * 1º primeira resolução e apenas para a txtbox dos algarismos
+         * NOTA: Esta solução, quase funcional, não é a mais indicada pois não possibilita que a tecla backspace funcione,  que impossibilita a eliminação do conteudo 
+         * das textbox. Apenas com recurso á tecla DEL é que é possivel eliminar o conteudo
+         */
+
+        /*
+         * abaixo encontra-se um evento que contem dois paramatros. "sender" que é a identificação do proprio objeto, que neste
+         * caso é a textBox ("txt_algarismos"); "e" que diz respeito a um consjunto de informações realacionadas com 
+         * os eventos que vao acontecer;
+        */
+
+        /*
+        private void txt_algarismos_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string chars = "0123456789";
+            //keyChar: caracter que foi pressionado
+            if (!chars.Contains(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                e.Handled = false;
+            } 
+        }
+        */
+
+
+        /*
+         * ABAIXO A REOLUÇÃO MAIS INDICADA E COMPLETAMENTE FUNCIONAL E JÁ COM A VALIDAÇÃO DAS DUAS txtbox
+         *
+         * Abaixo encontra-se um evento que contem dois paramatros. 
+         * - "sender" que é a identificação do proprio objeto, que neste caso é a textBox ("txt_algarismos"); 
+         * - "e" que diz respeito a um consjunto de informações realacionadas com os eventos que vao acontecer;
+         */
+
+        /*
+         * o metodo a baixo obtem-se selecionando o objeto em questão neste caso a textbox (txt_algarismos)e no menu evenos selecionar com duplo click 
+         * a propriedade "keyPress"
+         */
+        private void txt_algarismos_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) //keyChar: caracter que foi pressionado
+            { //se o caracter for diferente do controlo(BACKSPACE) e se o char for numero
+                e.Handled = true; //Handled = manipulação; Neste caso, o evento de "inserção" é autorizado(true) 
+            }
+        }
+
+
+
+        private void txt_letras_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && //caso não seja selecionado um backspace por exemplo
+                !char.IsLetter(e.KeyChar) && //caso não seja uma letra
+                !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
